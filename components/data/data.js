2630f49e5530220ba2d00a5f172e72699468b803
RohithVY <venkatrohith1998@gmail.com>
2023-07-13 20:32:01 +0530

    Created Saperate file containing all mappable data

---
+++ components/data/data.js
@@ -0,0 +1,69 @@
+export const highlights = [
+  {
+    id: 1,
+    title: "Transformative Results",
+    description:
+      "Our personalised consultation sessions provide revolutionary advantages, enabling exceptional personal progress with direction and support.",
+    logo: "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/13b0f11a-multor-benefit1-icon.svg",
+  },
+  {
+    id: 2,
+    title: "Enhanced Well-being",
+    description:
+      "Investigate how our consulting sessions improve well-being by addressing your requirements and implementing practical plans for a satisfying life.",
+    logo: "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/8ab92ea8-multor-benefit2-icon.svg",
+  },
+  {
+    id: 3,
+    title: "Seamless Experience",
+    description:
+      "Experience a seamless and convenient journey towards self-improvement when signing up and participating in our consultation sessions.",
+    logo: "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/c32dd90d-multor-benefit3-icon.svg",
+  },
+];
+
+export const individualTestimonials = [
+  {
+    id: "test__1",
+    rating:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/dfb61e74-multor-testimonial1-stars.svg",
+    message:
+      "Multor's guidance and support have helped me gain clarity, overcome obstacles, and achieve remarkable personal growth. I highly recommend these sessions to everyone.",
+    name: "Sarah",
+    image:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/52bdc545-multor-testimonial1-headshot_100000000000000000001o.jpg",
+    location: "Panaji, Goa",
+  },
+  {
+    id: "test__2",
+    rating:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/6d441dac-multor-testimonial2-stars.svg",
+    message:
+      "the process was seamless and user-friendly, allowing me to dive right into the transformative experience without any hassle",
+    name: "Jennifer",
+    image:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/94045dbc-multor-testimonial2-headshot_100000000000000000001o.jpg",
+    location: "New York, USA",
+  },
+];
+
+export const features = [
+  {
+    id: "f1",
+    image: "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/53ad3716-multor-feature1-image_100000000000000000001o.jpg",
+    heading: "Exceptional Guidance",
+    message: "Our offer provides exceptional value by delivering meaningful and tangible benefits that enhance people's lives."
+  },
+  {
+    id: "f2",
+    image: "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/adf66a57-multor-feature2-image_100000000000000000001o.jpg",
+    heading: "Elevate Efficiency, Boost Productivity.",
+    message: "Empower your team to achieve more with our on-time guidance, allowing you to maximize productivity and drive results."
+  },
+  {
+    id: "f3",
+    image: "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/6d486c61-multor-feature3-image_100000000000000000001o.jpg",
+    heading: "Personalized Solutions for Unparalleled Experiences",
+    message: "Delight your customers with tailored experiences that leave a lasting impression and create meaningful connections."
+  }
+]
